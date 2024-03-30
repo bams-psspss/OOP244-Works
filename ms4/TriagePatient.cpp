07aa90f23a72d6b793f841851929b6b78b5480c6
Pattarawan Saravaneeyawong <psaravaneeyawong@myseneca.ca>
2024-03-29 23:08:49 -0400

    Forgot to include citation part

--- ms4/TriagePatient.cpp
+++ ms4/TriagePatient.cpp
@@ -1,3 +1,22 @@
+/* Citation and Sources...
+Final Project Milestone 4
+Module: TriagePatient
+Filename: TriagePatient.cpp
+Version 1.1
+Author   Pattarwan Saravaneeyawong
+Revision History
+-----------------------------------------------------------
+Date					Reason
+29 / 03 / 2024			I forgot to include the citation part
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------
+OR
+-----------------------------------------------------------
+Write exactly which part of the code is given to you as help and
+who gave it to you, or from what source you acquired it.
+-----------------------------------------------------------*/
 #include <iostream>
 #include "TriagePatient.h"
 
